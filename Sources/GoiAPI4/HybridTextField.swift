9957da2d1876d9f62148a7474b560d139b3354d9
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-26 12:16:58 +0700

    save

--- Sources/GoiAPI4/HybridTextField.swift
+++ Sources/GoiAPI4/HybridTextField.swift
@@ -6,27 +6,32 @@ import UniformTypeIdentifiers
 ///Contains all the code for the Secure and regular TextFields
 struct HybridTextField: View {
     @Binding var text: String
+    @State var backupText =  ""
     @State var isSecure: Bool = true
     var titleKey: String
     var body: some View {
         HStack{
-            Group{
-                if isSecure{
-                    SecureField(titleKey, text: $text)
-                    
-                }else{
-                    TextEditor(text: $text)
-                }
-            }
+            
+                TextEditor(text: $text)
+            
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
             .foregroundColor(Color.blue)
             .background(Color.gray)
             .cornerRadius(5)
-            .textFieldStyle(.roundedBorder)
-                .animation(.easeInOut(duration: 0.2), value: isSecure)
+           
             //Add any common modifiers here so they dont have to be repeated for each Field
             Button(action: {
                 isSecure.toggle()
+                if(isSecure == true){
+                    backupText = text
+                    self.text = String(text.map { char in
+                        return "*"
+                    })
+                }
+                else
+                {
+                    text = backupText
+                }
             }, label: {
                 Image(systemName: !isSecure ? "eye.slash" : "eye" )
             })
