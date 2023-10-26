84422bc411d54d0240dbbc2c57236c0062869076
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-26 11:22:38 +0700

    save

--- Sources/GoiAPI4/RecoveryWalletView.swift
+++ Sources/GoiAPI4/RecoveryWalletView.swift
@@ -8,7 +8,8 @@ import UniformTypeIdentifiers
 public struct RecoveryWalletView: View {
    
   
-    
+    @State var isShowOrHideTextOf12Seed  = true
+    @State var string12SeedText = ""
     public init() {
         
     }
@@ -17,10 +18,29 @@ public struct RecoveryWalletView: View {
         NavigationView {
             //Choose View
             VStack(alignment: .center) {
-                Text("GOI API 4")
+                Text("Recovery Your Wallet from 12 seed words").font(.title)
+                HStack{
+                    Text("Secret Recovery Words")
+                    Spacer()
+                    //nut show và hide các từ seeds mà user nhập vào
+                    Button(action: {
+                        
+                        
+                    }) {
+                        VStack {
+                            Text(isShowOrHideTextOf12Seed ? "Show" : "Hide")
+                        }
+                        .padding()
+                        .accentColor(Color(.systemBlue))
+                    }
+                }
+                HStack{
+                    TextField("Enter your 12 seeds separated by spaces", text: $string12SeedText)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+                }
                
             }
-            .padding(.bottom,50)
+            .padding(.bottom,10)
             
         }
         //
