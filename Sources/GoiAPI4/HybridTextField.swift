feab0bda53cfcea0fc42eec9a37765c36cfbea15
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-26 11:52:04 +0700

    save

--- Sources/GoiAPI4/HybridTextField.swift
+++ Sources/GoiAPI4/HybridTextField.swift
@@ -9,34 +9,29 @@ struct HybridTextField: View {
     @State var isSecure: Bool = true
     var titleKey: String
     var body: some View {
-        HStack{
-            Group{
-                if isSecure{
-                    SecureField(titleKey, text: $text)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
-                        .foregroundColor(Color.white)
-                        .background(Color.gray)
-                        .cornerRadius(5)
-                }else{
-                    TextField(titleKey, text: $text)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
-                        .foregroundColor(Color.white)
-                        .background(Color.gray)
-                        .cornerRadius(5)
-                }
-            }.textFieldStyle(.roundedBorder)
-                .animation(.easeInOut(duration: 0.2), value: isSecure)
+        
+        
+        if isSecure{
+            SecureField(titleKey, text: $text)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+                .foregroundColor(Color.white)
+                .background(Color.gray)
+                .cornerRadius(5)
+        }else{
+            TextField(titleKey, text: $text)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+                .foregroundColor(Color.white)
+                .background(Color.gray)
+                .cornerRadius(5)
+            
             //Add any common modifiers here so they dont have to be repeated for each Field
             Button(action: {
                 isSecure.toggle()
             }, label: {
                 Image(systemName: !isSecure ? "eye.slash" : "eye" )
             })
-        }//Add any modifiers shared by the Button and the Fields here
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
-        .foregroundColor(Color.white)
-        .background(Color.gray)
-        .cornerRadius(5)
+            
+        }
     }
 }
 
