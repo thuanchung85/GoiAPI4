9d2110362baedf27daa0f81de2b3bd9fda30f010
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-26 12:02:26 +0700

    save

--- Sources/GoiAPI4/HybridTextField.swift
+++ Sources/GoiAPI4/HybridTextField.swift
@@ -9,32 +9,28 @@ struct HybridTextField: View {
     @State var isSecure: Bool = true
     var titleKey: String
     var body: some View {
-        
-        
-        if isSecure{
-            SecureField(titleKey, text: $text)
-                .multilineTextAlignment(.leading)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
-                .foregroundColor(Color.white)
-                .background(Color.gray)
-                .cornerRadius(5)
-                .lineLimit(nil)
-        }else{
-            TextField(titleKey, text: $text)
-                .multilineTextAlignment(.leading)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
-                .foregroundColor(Color.white)
-                .background(Color.gray)
-                .cornerRadius(5)
-                .lineLimit(nil)
-        }
-        //Add any common modifiers here so they dont have to be repeated for each Field
-        Button(action: {
-            isSecure.toggle()
-        }, label: {
-            Image(systemName: !isSecure ? "eye.slash" : "eye" )
-        })
-        
+        HStack{
+            Group{
+                if isSecure{
+                    SecureField(titleKey, text: $text)
+                    
+                }else{
+                    TextEditor(text: $text)
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+            .foregroundColor(Color.white)
+            .background(Color.gray)
+            .cornerRadius(5)
+            .textFieldStyle(.roundedBorder)
+                .animation(.easeInOut(duration: 0.2), value: isSecure)
+            //Add any common modifiers here so they dont have to be repeated for each Field
+            Button(action: {
+                isSecure.toggle()
+            }, label: {
+                Image(systemName: !isSecure ? "eye.slash" : "eye" )
+            })
+        }//Add any modifiers shared by the Button and the Fields here
     }
 }
 
