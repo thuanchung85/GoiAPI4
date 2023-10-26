fce306f584abcf94601928f8d20873c1febc0e73
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-26 14:25:40 +0700

    save

--- Sources/GoiAPI4/RecoveryWalletView.swift
+++ Sources/GoiAPI4/RecoveryWalletView.swift
@@ -22,15 +22,29 @@ public struct RecoveryWalletView: View {
             VStack(alignment: .center) {
                 Text("Recovery Your Wallet from 12 seed words").font(.title)
               
-                Text("Secret Recovery Words")
-                   
+                Text("Enter your 12 seeds separated by spaces:")
+                
+                //text editor cho phep user nhập 12 từ và che dấu khi cần
                 HStack{
                     HybridTextField(text: $string12SeedText, textHide: "", bkt: "", titleKey: "Enter your 12 seeds separated by spaces")
                        
-                   
-                  
                 }
                
+                //nut next
+                Button(action: {
+                   
+                    
+                }) {
+                    VStack {
+                        Text("NEXT").foregroundColor(Color.red)
+                    }
+                    .padding()
+                    .accentColor(Color(.red))
+                    .cornerRadius(4.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
+                    )
+                }
             }
             .padding(.bottom,10)
             
