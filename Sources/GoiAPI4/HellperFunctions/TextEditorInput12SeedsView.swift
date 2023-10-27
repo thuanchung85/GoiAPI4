9dd7e1080239959f9a4387a95d24abf5480d49db
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-27 10:02:12 +0700

    save

--- Sources/GoiAPI4/HellperFunctions/TextEditorInput12SeedsView.swift
+++ Sources/GoiAPI4/HellperFunctions/TextEditorInput12SeedsView.swift
@@ -141,7 +141,8 @@ struct TextEditorInput12SeedsView: View {
 
 func traRaChuoi(data12Words:[String], text:String)->[String]
 {
-    let arrT  = text.components(separatedBy: " ")
+    var arrT  = text.components(separatedBy: " ")
+    if(arrT.last == "") {arrT.removeLast()}
     if(arrT.count<=12){
         let  combin2Array = Array(zip(data12Words, arrT))
         let arrRWithIndex = combin2Array.enumerated()
@@ -151,24 +152,7 @@ func traRaChuoi(data12Words:[String], text:String)->[String]
         
         return arrX
     }
-    else{
-        //trường hợp user chỉ vô tình nhập 1 từ space thì vẫn ok
-        if(arrT.count == 13){
-            if (arrT.last == "")
-            {
-                let  combin2Array = Array(zip(data12Words, arrT))
-                let arrRWithIndex = combin2Array.enumerated()
-                let arrX = arrRWithIndex.map({ eO in
-                    return "\(eO.offset + 1). " + eO.element.1
-                })
-                
-                return arrX
-            }
-            else{
-                return ["You input over 12 words, please check again!"]
-            }
-        }
-    }
+   
     return ["You input over 12 words, please check again!"]
 }
 
