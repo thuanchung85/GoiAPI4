1480d2bd2fb145ab8279fc7c220b4c7c9184dc58
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-30 15:36:05 +0700

    save

--- Sources/GoiAPI4/Input12SeedsView/Input12SeedsView.swift
+++ Sources/GoiAPI4/Input12SeedsView/Input12SeedsView.swift
@@ -170,7 +170,9 @@ struct Input12SeedsView: View {
                                     .cornerRadius(20)
                                     .overlay(
                                         RoundedRectangle(cornerRadius: 20)
-                                            .stroke(.green, lineWidth: 1)
+                                            .stroke(style: StrokeStyle(lineWidth: 1, dash: [15]))
+                                                       .foregroundColor(Color(UIColor.blue))
+                                        
                                     )
                             }
                         }
