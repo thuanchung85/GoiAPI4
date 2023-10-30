f7b51d422515fb80a57b41ff29fd975052f3d4d3
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-30 17:18:21 +0700

    save

--- Sources/GoiAPI4/OldWalletView/OldWalletView.swift
+++ Sources/GoiAPI4/OldWalletView/OldWalletView.swift
@@ -66,7 +66,7 @@ public struct OldWalletView: View {
                                         .padding(.bottom,10)
                                         
                                     }
-                                
+                                    .padding(10)
                         }
                     
                 }
